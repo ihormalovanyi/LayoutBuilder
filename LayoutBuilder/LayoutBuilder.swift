b6610e6fb181f55ee8b810d74c636051eab73473
Ihor M <gethwalk@gmail.com>
2021-05-04 22:44:34 +0300

    [update] first minor release

--- LayoutBuilder/LayoutBuilder.swift
+++ LayoutBuilder/LayoutBuilder.swift
@@ -5,58 +5,49 @@
 //  Created by Ihor Malovanyi on 04.05.2021.
 //
 
-#if os(macOS)
-import Cocoa
-#else
-import UIKit
+@_exported import LayoutBuilder
+
+#if canImport(UIKit)
+    import UIKit
+    public typealias View = UIView
+    public typealias LayoutPriority = UILayoutPriority
+#elseif canImport(AppKit)
+    import AppKit
+    public typealias View = NSView
+    public typealias LayoutPriority = NSLayoutConstraint.Priority
 #endif
 
 //https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/ProgrammaticallyCreatingConstraints.html#//apple_ref/doc/uid/TP40010853-CH16-SW1
 //Example: item1.attribute = multiplier * item2.attrubute + constant
 
-#if os(macOS)
-public typealias LayoutParts = (view: NSView, attribute: NSLayoutConstraint.Attribute, constant: CGFloat, multiplier: CGFloat)
-
-public extension NSView {
-    
-    func layout(_ value: NSLayoutConstraint.Attribute) -> LayoutParts { (self, value, 0, 1) }
-    
-}
-#else
-public typealias LayoutParts = (view: UIView, attribute: NSLayoutConstraint.Attribute, constant: CGFloat, multiplier: CGFloat)
-
-public extension UIView {
-    
-    func layout(_ value: NSLayoutConstraint.Attribute) -> LayoutParts { (self, value, 0, 1) }
-    
-}
-#endif
+//MARK: Operators
 
 infix operator ! : AdditionPrecedence
 public func +(lhs: LayoutParts, rhs: CGFloat) -> LayoutParts { (lhs.view, lhs.attribute, rhs, lhs.multiplier) }
 public func -(lhs: LayoutParts, rhs: CGFloat) -> LayoutParts { (lhs.view, lhs.attribute, -rhs, lhs.multiplier) }
 public func *(lhs: CGFloat, rhs: LayoutParts) -> LayoutParts { (rhs.view, rhs.attribute, rhs.constant, lhs) }
-#if os(macOS)
-public func !(lhs: NSLayoutConstraint, rhs: NSLayoutConstraint.Priority) -> NSLayoutConstraint {
+public func !(lhs: NSLayoutConstraint, rhs: LayoutPriority) -> NSLayoutConstraint {
     let constraint = lhs
     constraint.priority = rhs
     return constraint
 }
-#else
-public func !(lhs: NSLayoutConstraint, rhs: UILayoutPriority) -> NSLayoutConstraint {
-    let constraint = lhs
-    constraint.priority = rhs
-    return constraint
-}
-#endif
 
-//Layout Attributes
 public func ==(lhs: LayoutParts, rhs: CGFloat) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .equal) }
 public func ==(lhs: LayoutParts, rhs: LayoutParts) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .equal) }
 public func >=(lhs: LayoutParts, rhs: CGFloat) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .greaterThanOrEqual) }
 public func >=(lhs: LayoutParts, rhs: LayoutParts) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .greaterThanOrEqual) }
 public func <=(lhs: LayoutParts, rhs: CGFloat) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .lessThanOrEqual) }
 public func <=(lhs: LayoutParts, rhs: LayoutParts) -> NSLayoutConstraint { createConstraint(lhs: lhs, rhs: rhs, relation: .lessThanOrEqual) }
+
+//MARK: Support logic
+
+public typealias LayoutParts = (view: View, attribute: NSLayoutConstraint.Attribute, constant: CGFloat, multiplier: CGFloat)
+
+public extension View {
+    
+    func layout(_ value: NSLayoutConstraint.Attribute) -> LayoutParts { (self, value, 0, 1) }
+    
+}
 
 internal func createConstraint(lhs: LayoutParts, rhs: LayoutParts, relation: NSLayoutConstraint.Relation) -> NSLayoutConstraint {
     .init(item: lhs.view, attribute: lhs.attribute, relatedBy: relation, toItem: rhs.view, attribute: rhs.attribute, multiplier: 1, constant: rhs.constant)
@@ -69,31 +60,33 @@ internal func createConstraint(lhs: LayoutParts, rhs: CGFloat, relation: NSLayou
     return .init(item: lhs.view, attribute: lhs.attribute, relatedBy: relation, toItem: lhs.view.superview, attribute: lhs.attribute, multiplier: 1, constant: rhs)
 }
 
-//protocol ConstraintsGroup {
-//
-//    var constraints: [NSLayoutConstraint] { get }
-//
-//}
-//
-//extension NSLayoutConstraint: ConstraintsGroup {
-//
-//    public var constraints: [NSLayoutConstraint] { [self] }
-//
-//}
-//
-//extension Array: ConstraintsGroup where Element == NSLayoutConstraint {
-//
-//    public var constraints: [NSLayoutConstraint] { self }
-//
-//}
+public protocol ConstraintsGroup {
+
+    var constraints: [NSLayoutConstraint] { get }
+
+}
+
+extension NSLayoutConstraint: ConstraintsGroup {
+
+    public var constraints: [NSLayoutConstraint] { [self] }
+
+}
+
+extension Array: ConstraintsGroup where Element == NSLayoutConstraint {
+
+    public var constraints: [NSLayoutConstraint] { self }
+
+}
+
+//MARK: Layout Result Builder
 
 @resultBuilder
 public enum LayoutResultBuilder {
     
-    public static func buildBlock(_ components: NSLayoutConstraint...) -> [NSLayoutConstraint] { components }
-    public  static func buildOptional(_ components: [NSLayoutConstraint]?) -> [NSLayoutConstraint] { components ?? [] }
-    public static func buildEither(first components: [NSLayoutConstraint]) -> [NSLayoutConstraint] { components }
-    public static func buildEither(second components: [NSLayoutConstraint]) -> [NSLayoutConstraint] { components }
+    public static func buildBlock(_ components: ConstraintsGroup...) -> [NSLayoutConstraint] { components.flatMap(\.constraints) }
+    public  static func buildOptional(_ components: [ConstraintsGroup]?) -> [NSLayoutConstraint] { components?.flatMap(\.constraints) ?? [] }
+    public static func buildEither(first components: [ConstraintsGroup]) -> [NSLayoutConstraint] { components.flatMap(\.constraints) }
+    public static func buildEither(second components: [ConstraintsGroup]) -> [NSLayoutConstraint] { components.flatMap(\.constraints) }
     
 }
 
@@ -103,11 +96,7 @@ public struct Layout {
     public init(@LayoutResultBuilder _ build: () -> [NSLayoutConstraint]) {
         let constraints = build()
         constraints.forEach {
-            #if os(macOS)
-            ($0.firstItem as? NSView)?.translatesAutoresizingMaskIntoConstraints = false
-            #else
-            ($0.firstItem as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
-            #endif
+            ($0.firstItem as? View)?.translatesAutoresizingMaskIntoConstraints = false
         }
         NSLayoutConstraint.activate(constraints)
     }
